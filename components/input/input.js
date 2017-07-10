6094d0a092b1663452dbfe0ca08e3b1d61761162
Andrew Robida <afrobida@me.com>
2017-07-09 19:32:20 -0700

    first commit

---
+++ components/input/input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// CSS, text, container, something to type content in, button emulator
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+
+//
+//
+export default class Input extends React.components {
+
+    render() {
+        // return data, i.e. result of executing function
+        return (
+            <View>
+            
+            </View>
+        )
+    }
+}
